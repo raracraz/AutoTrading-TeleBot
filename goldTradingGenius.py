9c7136f99485888201900efce7f077d57b556ace
raynac <ryan.aaixe@gmail.com>
2023-10-06 03:00:33 +0800

    update

--- goldTradingGenius.py
+++ goldTradingGenius.py
@@ -3,6 +3,8 @@ from telegram import Update
 from telegram.ext import CallbackContext, MessageHandler, Filters, Updater
 import logging
 import time
+import re
+import MetaTrader5 as mt5
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -16,24 +18,125 @@ my_user_id = int(config.get('Telegram', 'my_user_id'))  # Add your_user_id to yo
 
 token = config.get('TradingBot', 'token')
 
-def handle_message(update: Update, context: CallbackContext) -> None:
-    print(f"Received update: {update}")
-    # Check if the update contains a message object
-    if not update.message:
-        print("Received an update without a message")
-        return
+mt5_login = int(config.get('MetaTrader', 'login'))
+mt5_password = config.get('MetaTrader', 'password')
+mt5_server = config.get('MetaTrader', 'server')
 
-    # Print the received message to the terminal
-    print(f"Received message: {update.message.text}")
+# Initialize MT5 connection with login credentials
+if not mt5.initialize(login=mt5_login, password=mt5_password, server=mt5_server):
+    print("initialize() failed, error code =",mt5.last_error())
+    quit()
+
+# Now you're connected. You can fetch account information, place trades, etc.
+print(mt5.account_info())
+
+def handle_message(update: Update, context: CallbackContext) -> None:
+    text = None
     
-    context.bot.send_message(my_user_id, f"Received a new message")
+    # Check if the update contains a regular message
+    if update.message:
+        text = update.message.text
+    # Check if the update contains a channel post
+    elif update.channel_post:
+        text = update.channel_post.text
     
-    # Extract the text from the message
-    text = update.message.text
+    # If no text was found, return
+    if not text:
+        return
     
-    if text.startswith("🔷XAUUSD GOLD"):
+    if text.startswith("🔷"):
         print(f"Received matching message: {text}")
         context.bot.send_message(my_user_id, f"Received a new matching message: {text}")
+        info = extract_order_info(text)
+        
+        # Check for TP targets in order: tp3, tp2, tp1
+        tp_target = info.get('tp3') or info.get('tp2') or info.get('tp1')
+        
+        if tp_target:
+            place_limit_order(info['symbol'], info['order_type'], 0.01, info['order_price'], info['sl'], tp_target)
+        else:
+            print("No TP target found!")
+        
+        print(info)
+
+def extract_order_info(text: str) -> dict:
+    results = {}
+
+    # Extract the symbol (e.g., XAUUSD, GBPJPY)
+    symbol_match = re.search(r'🔷(\w+)', text)
+    if symbol_match:
+        results['symbol'] = symbol_match.group(1)
+
+    # Extract order type (Buy/Sell)
+    order_type_match = re.search(r'(BUY|SELL)', text, re.IGNORECASE)
+    if order_type_match:
+        results['order_type'] = order_type_match.group(1).upper()
+
+    # Extract order price.
+    price_match = re.search(r'(BUY|SELL)\s+(\d+\d+)(?:\/)(\d+\d+)', text, re.IGNORECASE)
+    if price_match:
+        results['order_price'] = float(price_match.group(2))
+
+    # Extract SL value.
+    sl_match = re.search(r'\s*SL\d*\s*(\d+\d+)', text)
+    if sl_match:
+        results['sl'] = float(sl_match.group(1))
+
+    
+    # Extract TP values.
+    tp_matches = re.findall(r'\s*TP\d*\s*(\d+\d+)', text)
+    for index, tp_value in enumerate(tp_matches, start=1):
+        key = f"tp{index}"
+        results[key] = float(tp_value)
+
+    return results
+
+def place_limit_order(symbol, action, volume, price, sl, tp):
+    current_price = mt5.symbol_info(symbol).bid  # or .ask, depending on your needs
+
+    if action == "BUY":
+        if price > current_price:  # If the BUY limit is above the current price
+            order_type = mt5.ORDER_TYPE_BUY_STOP
+        else:
+            order_type = mt5.ORDER_TYPE_BUY_LIMIT
+    elif action == "SELL":
+        if price < current_price:  # If the SELL limit is below the current price
+            order_type = mt5.ORDER_TYPE_SELL_STOP
+        else:
+            order_type = mt5.ORDER_TYPE_SELL_LIMIT
+    else:
+        print(f"Unknown action: {action}")
+        return
+    
+    # Create the request
+    request = {
+        "action": mt5.TRADE_ACTION_PENDING,
+        "symbol": symbol,
+        "volume": volume,
+        "type": order_type,
+        "price": price,
+        "sl": sl,
+        "tp": tp,
+        "magic": 123456,  # Magic number, can be any identifier you choose
+        "comment": "python script open",  # Comment on the order
+        "type_time": mt5.ORDER_TIME_GTC,  # Good Till Cancelled
+        "type_filling": mt5.ORDER_FILLING_RETURN,
+    }
+
+    # Send the request
+    result = mt5.order_send(request)
+    
+    if result is None:
+        print("Failed to send order. No response received.")
+        error = mt5.last_error()
+        print("Error in order_send(): ", error)
+        return
+
+    if result.retcode != mt5.TRADE_RETCODE_DONE:
+        print(f"Failed to send order. Error: {result.comment}")
+        return
+    print(f"Order successfully placed with ticket {result.order}")
+    return result.order
 
 
 # Create an updater and pass your bot's token
